b4eae5ae119a260d16a16e3674da0a00fd0d90a1
pallovitor <pvitor.asilva@gmail.com>
2024-03-12 19:31:28 -0300

    adicionado bitcoin e libra

--- scripts.js
+++ scripts.js
@@ -28,8 +28,11 @@ window.oninput = function (event) {
     const currencyValueConverted = document.querySelector(".currency-value");
     const currencyCurrency = document.querySelector(".currency-currency");
     const currencyImg = document.querySelector(".currency-img");
-    const dolarDay = 5.2;
-    const euroDay = 5.44;
+
+    const dolarDay = 4.97;
+    const euroDay = 5.43;
+    const bitcoinDay = 352309.32;
+    const libraDay = 6.36;
 
     if (currencySelect.value == "euro") {
         currencyCurrency.innerHTML = "Euro";
@@ -46,6 +49,23 @@ window.oninput = function (event) {
             style: "currency",
             currency: "USD",
         }).format(inputCurrencyValues / dolarDay);
+    }
+    if (currencySelect.value == "bitcoin") {
+        currencyCurrency.innerHTML = "Bitcoin";
+        currencyImg.src = "./assets/bitcoin.png";
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BTC",
+            minimumFractionDigits: 8,
+        }).format(inputCurrencyValues / bitcoinDay);
+    }
+    if (currencySelect.value == "libra") {
+        currencyCurrency.innerHTML = "Libra";
+        currencyImg.src = "./assets/libra.png";
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-EU", {
+            style: "currency",
+            currency: "GBP",
+        }).format(inputCurrencyValues / libraDay);
     }
 
     if (campo == "input-currency") {
